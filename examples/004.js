3f21c5270c9674ef3a4e50661c2b3e1165eca903
Laurent De Marez <laurent@demarez.org>
2024-04-08 10:03:23 +0200

    Add example ISO 8583 messages.

---
+++ examples/004.js
@@ -0,0 +1,24 @@
+/*
+  Authorization request for an online purchase.
+    - 1 EUR
+    - Card on file
+    - Ecom purchase
+ */
+
+export const ISO_MSG = {
+  DE3: "010000",
+  DE4: "000000000100",
+  DE6: "000000000100",
+  DE7: "0216103818",
+  DE11: "000090",
+  DE12: "103626",
+  DE13: "0216",
+  DE14: "2509",
+  DE18: "5999",
+  DE19: "276",
+  DE22: "100",
+  DE25: "59",
+  DE43: "CRV*trainline London G   Brussels     LT",
+  DE49: "978",
+  DE51: "978",
+};
